1975e3f29af3a865f38c0c761edeee4960ac5eca
Alex A.F <alex2andres@gmail.com>
2020-02-21 21:51:01 +0100

    Testing: Some basics components

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -14,7 +14,10 @@ class MainScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello from component</Text>
+                <Text>ToDo List App</Text>
+                <Text>Tareas 1</Text>
+                <Text selectable selectionColor="#008bef">Tarea 2 - This is selectable with custom color.</Text>
+                <Text>Tarea 3 </Text>
             </View>
         );
     }
