de09c56392180846e4ca04783f63040f38633f66
Alex A.F <alex2andres@gmail.com>
2020-03-06 20:43:15 +0100

    Test inline styles

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 const TodoList = ({ todos }) => (
     <Fragment>
         {todos.map(
-            todo => (!todo.done && <Text key={todo.text}>{todo.text}</Text>
+            todo => (!todo.done && <Text style={{borderWidth:0.1,padding:2,margin:2}} key={todo.text}>{todo.text}</Text>
             ))}
     </Fragment>
 );
