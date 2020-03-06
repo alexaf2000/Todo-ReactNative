cfe0fb1723793a2703c5901610ce29bfd783004a
Alex A.F <alex2andres@gmail.com>
2020-03-06 20:52:57 +0100

    Style testing

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,9 +1,26 @@
 import React, { Fragment } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+    listItem: {
+        borderWidth: 1,
+        padding: 2,
+        margin: 2,
+        width: "80%"
+    },
+    text:{
+        padding: 2,
+        fontWeight: "bold"
+    }
+})
 const TodoList = ({ todos }) => (
     <Fragment>
         {todos.map(
-            todo => (!todo.done && <Text style={{borderWidth:0.1,padding:2,margin:2}} key={todo.text}>{todo.text}</Text>
+            todo => (!todo.done &&
+                <View key={todo.text} style={styles.listItem}>
+                    <Text style={styles.text}>
+                        {todo.text}
+                    </Text>
+                </View>
             ))}
     </Fragment>
 );
