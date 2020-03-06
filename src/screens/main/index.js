3839ebd4191b56f789d2f13c4e8cbfe5e6879771
Alex A.F <alex2andres@gmail.com>
2020-03-06 20:39:22 +0100

    State example

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -10,12 +10,20 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
-
-const todos = [
-    { text: "Thing 1", done: false }, { text: "Thing 2", done: false }, { text: "Thing 3", done: true },
-]
 class MainScreen extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            todos: [
+                { text: "Thing 1", done: false },
+                { text: "Thing 2", done: false },
+                { text: "Thing 3", done: true }
+            ]
+        };
+    };
     render() {
+        const { todos } = this.state;
         return (
             <View style={styles.container}>
                 <Text selectable selectionColor="red">ToDo List App</Text>
