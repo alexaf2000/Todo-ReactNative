0d0d4adc847617df663e4533bce64859ed3660dc
Alex A.F <alex2andres@gmail.com>
2020-03-10 00:10:23 +0100

    Button style and add function without functionality

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {View, Text, StyleSheet, SafeAreaView, TextInput} from "react-native";
+import {View, Text, StyleSheet, SafeAreaView, TextInput, Button} from "react-native";
 import TodoList from "TodoList/src/components/TodoList";
 import { getTodos } from "TodoList/src/data/todos"
 
@@ -11,6 +11,15 @@ const styles = StyleSheet.create({
         alignItems: 'center'
         // justifyContent: 'center',
     },
+    inputTodo:{
+        flexDirection: "row",
+        width: "80%",
+        marginBottom: 5
+    },
+    text:{
+        flex: 1,
+        borderBottomWidth: 1
+    }
 });
 class MainScreen extends Component {
     constructor(props) {
@@ -24,12 +33,18 @@ class MainScreen extends Component {
     componentDidMount = () => {
         this.setState({ todos: getTodos() });
     }
+    addTodo = () => {
+        
+    }
     render() {
         const { todos,newTodo } = this.state;
         return (
             <SafeAreaView style={styles.container}>
                 <Text selectable selectionColor="red" style={{ fontSize: 24, marginBottom: 25, fontWeight: "bold" }}>ToDo List App</Text>
-                <TextInput value={newTodo} onChangeText={thingtodo=> this.setState({newTodo:thingtodo})} placeholder="Introduce una nueva tarea" autoCapitalize="sentences" style={styles.text}/>
+                <View style={styles.inputTodo}>
+                <TextInput value={newTodo} onChangeText={thingtodo=> this.setState({newTodo:thingtodo})} placeholder="Introduce una nueva tarea" autoCapitalize="sentences" style={styles.text} returnKeyType="done"/>
+                <Button title="Añadir" onPress={this.addTodo}/>
+                </View>
                 <TodoList todos={todos} />
                 <Text>{newTodo}</Text>
             </SafeAreaView>
